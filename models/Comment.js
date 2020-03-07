19f9a55b6c16a6ee0b2da7203b27b33058b18762
jiwoogae <60306777+jiwoogae@users.noreply.github.com>
2020-03-07 11:11:08 +0900

    6.4

--- models/Comment.js
+++ models/Comment.js
@@ -8,7 +8,12 @@ const CommentSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
   }
 });
 
 const model = mongoose.model("Comment", CommentSchema);
+export default model;
